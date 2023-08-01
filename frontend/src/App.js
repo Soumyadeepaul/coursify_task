76a54580bd2789ce8b3176c4d0da056a5c66b921
Soumyadeepaul <92711266+Soumyadeepaul@users.noreply.github.com>
2023-08-01 09:04:56 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Footer from './components/Footer';
+import Form from './components/Form';
+import Home from './components/Home'
+
+function App() {
+  return (
+    <>
+    <div className="row px-2" style={{width:'100%'}}>
+    <Home />
+    </div>
+    </>
+  );
+}
+
+export default App;
